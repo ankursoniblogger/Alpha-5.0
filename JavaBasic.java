da379a024d004b82d9e71c49c6fa8ef163269dd0
ankursoniblogger <ankursoniblogger@gmail.com>
2024-10-04 22:49:10 +0530

    Add first comment

---
+++ JavaBasic.java
@@ -0,0 +1,64 @@
+import java.util.* ;
+
+public class JavaBasic {
+    public static void main (String args[]){
+    //    Scanner sc = new Scanner(System.in);
+    //    int input = sc.nextInt();
+    //    System.out.print(input);
+    // if(input > 0){
+    //     System.out.print("Positive");
+    // } else {
+    //     System.out.print("Negetive");
+    // }
+
+    // double temp = 100.2 ;
+    // if(temp> 100){
+    //     System.out.print("You have a temp");
+    // } else {
+    //     System.out.print("You have not a temp");
+    // }
+    // int number = 10;
+    // switch(number){
+    //     case 1 : System.out.print("Sunday");
+    //     break ;
+    //     case 2 : System.out.print("M");
+    //     break ;
+    //     case 3 : System.out.print("T");
+    //     break ;
+    //     case 4 : System.out.print("W");
+    //     break ;
+    //     case 5 : System.out.print("TH");
+    //     break ;
+    //     case 6 : System.out.print("FR");
+    //     break ;
+    //     case 7 : System.out.print("SAT");
+    //     default : System.out.println("next time");
+      
+    // }
+    // int year = 2000;
+    // if((year % 4 == 0 && year % 100 != 0) || year% 400 == 0){
+    //     System.out.print("leap year");
+    // } else {
+    //     System.out.print(" not leap year");
+    // }
+    // int number = 10;
+    // int i = 1;
+    // int sum = 0;
+    // while(i <= number){
+    //     System.err.println(i);
+    //     sum = sum + i ;
+    //     i++;
+    // }
+    // System.out.print(sum);
+    // int n = 4 ;
+    // for(int i= 1; i<=n ; i++){
+    //     System.out.println("****");
+    // }
+    // int n = 10;
+    // for(int i= 10; i>=1; i--){
+    //     System.out.println(i);
+    // }
+ 
+
+    }
+}
